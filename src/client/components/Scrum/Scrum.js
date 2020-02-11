5eb505aa40c777eeb815388693203c6a6db4e917
이기승 <keeseung@sixshop.com>
2020-02-11 13:43:14 +0900

    style 스크럼 줄바꿈 추가

--- src/client/components/Scrum/Scrum.js
+++ src/client/components/Scrum/Scrum.js
@@ -12,19 +12,19 @@ class Scrum extends Component {
 
     return (
       <>
-        <div className='border-styles'>
+        <div className="border-styles">
           <p>{user.username}</p>
           <div>
             <p>1) 어제까지 한 일 </p>
-            <div>{user.content1}</div>
+            <div className="display-linebreak">{user.content1 || '-'}</div>
           </div>
           <div>
             <p>2) 오늘 할 일 </p>
-            <div>{user.content2}</div>
+            <div className="display-linebreak">{user.content2 || '-'}</div>
           </div>
           <div>
             <p>3) 일정 내 못 마칠 것 같은 일</p>
-            <div>{user.content3}</div>
+            <div className="display-linebreak">{user.content3 || '-'}</div>
           </div>
         </div>
       </>
