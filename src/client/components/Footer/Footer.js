190f9614943f91ce5036c6c18005a3092cce98af
이기승 <keeseung@sixshop.com>
2020-02-11 12:31:54 +0900

    feat Footer 아이콘 추가

--- src/client/components/Footer/Footer.js
+++ src/client/components/Footer/Footer.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+library.add(fab)
+
 
 const Footer = () => (
   <footer>
-    <hr />
-    <p>Footer</p>
+    <div className="container">
+      <div style={{textAlign: "center"}}>
+        <div style={{marginBottom: "8px"}}>
+          © (주)식스샵 ALL RIGHTS RESERVED.
+        </div>
+        <a style={{marginRight: "10px"}} href="https://github.com/mirrors89/daily-scrum" target="_blank">
+        <FontAwesomeIcon icon={["fab", "github"]} size="2x"/>
+        </a>
+        <a style={{marginRight: "10px"}} href="https://sixshop.atlassian.net/secure/RapidBoard.jspa?rapidView=1&projectKey=SS2" target="_blank">
+        <FontAwesomeIcon icon={["fab", "jira"]} size="2x"/>
+        </a>
+      </div>
+    </div>
   </footer>
 );
 
