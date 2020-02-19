bd4be1449d1b2b4e7beae09b0c131656f05d5baa
leolee <bong@sixshop.com>
2020-02-19 17:10:26 +0900

    네이게이션 추가, ui 살짝 보기 편하게만 우선 변경

--- src/client/components/Header/Header.js
+++ src/client/components/Header/Header.js
@@ -5,14 +5,21 @@ import { Link } from 'react-router-dom';
 const Header = () => (
   <header className="header">
     <h1>
-      <Link to="/">Sixshop Daily Scrum</Link>
+      <a class="" href="/">???</a>
     </h1>
-
     <nav>
-      <Link to="/helloworld">menu</Link>
+     <div class="container-fluid">
+      <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+        <ul class="nav navbar-nav">
+        <li class="active"><a href="#">모두 보기</a></li>
+        <li><a href="#">1) 어제까지 한 일</a></li>
+        <li><a href="#">2) 오늘 할 일</a></li>
+        <li><a href="#">3) 일정 내 못 마칠 것 같은 일</a></li>
+        <li><a href="#">내 스크럼 작성하기</a></li>
+        </ul>
+        </div>
+      </div>
     </nav>
-
-    <hr />
   </header>
 );
 
